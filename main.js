fef4777da71e01e38a6c1c0a3d847784439a8f65
Ryan Zhu <ryanxzhu@gmail.com>
2019-12-28 11:46:38 -0500

    changed randomize to randomizeArray

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var currentArray = [];
 var arrayLength = 10;
 var count = 0;
 
-randomize();
+randomizeArray();
 
-function randomize() {
+function randomizeArray() {
 	arrayToSort = [];
 
 	for (let i = 0; i < arrayLength; i++) {
@@ -36,8 +36,8 @@ function updateBars() {
 	}
 }
 
-randomizeBtn.addEventListener('mousedown', function() {
-	randomize();
+randomizeArrayBtn.addEventListener('mousedown', function() {
+	randomizeArray();
 	updateBars();
 });
 
@@ -55,24 +55,34 @@ resetBtn.addEventListener('mousedown', function() {
 // Selection sort button and functions
 
 selectionSortBtn.addEventListener('mousedown', function() {
+	count = 0;
 	selectionSort(arrayToSort);
+	count++;
 	updateBars();
+	console.log(count);
 });
 
 function swap(array, firstIndex, secondIndex) {
 	let temp = array[firstIndex];
+	count++;
 	array[firstIndex] = array[secondIndex];
+	count++;
 	array[secondIndex] = temp;
+	count++;
 }
 
 function indexOfMinimum(array, startIndex) {
 	let minValue = array[startIndex];
+	count++;
 	let minIndex = startIndex;
+	count++;
 
 	for (let i = minIndex + 1; i < array.length; i++) {
 		if (array[i] < minValue) {
 			minIndex = i;
+			count++;
 			minValue = array[i];
+			count++;
 		}
 	}
 
@@ -82,7 +92,9 @@ function indexOfMinimum(array, startIndex) {
 function selectionSort(array) {
 	for (let startIndex = 0; startIndex < array.length; startIndex++) {
 		let minIndex = indexOfMinimum(array, startIndex);
+		count++;
 		swap(array, startIndex, minIndex);
+		count++;
 	}
 }
 /*********************************************************************************************/
@@ -90,20 +102,26 @@ function selectionSort(array) {
 // Insertion sort button and functions
 
 insertionSortBtn.addEventListener('mousedown', function() {
+	count = 0;
 	insertionSort(arrayToSort);
+	count++;
 	updateBars();
+	console.log(count);
 });
 
 function insert(array, rightIndex, value) {
 	for (var i = rightIndex; i >= 0 && array[i] > value; i--) {
 		array[i + 1] = array[i];
+		count++;
 	}
 	array[i + 1] = value;
+	count++;
 }
 
 function insertionSort(array) {
 	for (let i = 0; i < array.length - 1; i++) {
 		insert(array, i, array[i + 1]);
+		count++;
 	}
 }
 
@@ -111,50 +129,73 @@ function insertionSort(array) {
 
 // Merge sort button and functions
 mergeSortBtn.addEventListener('mousedown', function() {
+	count = 0;
 	mergeSort(arrayToSort, 0, arrayToSort.length - 1);
-
+	count++;
 	updateBars();
+	console.log(count);
 });
 
 function merge(array, p, q, r) {
 	let lowHalf = [];
+	count++;
 	let highHalf = [];
+	count++;
 
 	let k = p;
+	count++;
 	let i = 0;
+	count++;
 	let j = 0;
+	count++;
 
 	for (i = 0; k <= q; i++, k++) {
 		lowHalf[i] = array[k];
+		count++;
 	}
 	for (j = 0; k <= r; j++, k++) {
 		highHalf[j] = array[k];
+		count++;
 	}
 
 	k = p;
+	count++;
 	i = 0;
+	count++;
 	j = 0;
+	count++;
 
 	while (i < lowHalf.length && j < highHalf.length) {
 		if (lowHalf[i] <= highHalf[j]) {
 			array[k] = lowHalf[i];
+			count++;
 			i++;
+			count++;
 		} else {
 			array[k] = highHalf[j];
+			count++;
 			j++;
+			count++;
 		}
 		k++;
+		count++;
 	}
 
 	while (i < lowHalf.length) {
 		array[k] = lowHalf[i];
+		count++;
 		k++;
+		count++;
 		i++;
+		count++;
 	}
 	while (j < highHalf.length) {
 		array[k] = highHalf[j];
+		count++;
 		k++;
+		count++;
 		j++;
+		count++;
 	}
 
 	return array;
@@ -163,9 +204,13 @@ function merge(array, p, q, r) {
 function mergeSort(array, p, r) {
 	if (p < r) {
 		let q = Math.floor((p + r) / 2);
+		count++;
 		mergeSort(array, p, q);
+		count++;
 		mergeSort(array, q + 1, r);
+		count++;
 		merge(array, p, q, r);
+		count++;
 	}
 }
 
@@ -174,29 +219,38 @@ function mergeSort(array, p, r) {
 // Quick sort button and functions
 
 quickSortBtn.addEventListener('mousedown', function() {
+	count = 0;
 	quickSort(arrayToSort, 0, arrayToSort.length - 1);
-
+	count++;
 	updateBars();
+	console.log(count);
 });
 
 // Uses swap() function from Selection sort functions
 
 function partition(array, p, r) {
 	let q = p;
+	count++;
 	for (let i = p; i < r; i++) {
 		if (array[i] <= array[r]) {
 			swap(array, q, i);
+			count++;
 			q++;
+			count++;
 		}
 	}
 	swap(array, q, r);
+	count++;
 	return q;
 }
 
 function quickSort(array, p, r) {
 	if (p < r) {
 		let indexOfPivot = partition(array, p, r);
+		count++;
 		quickSort(array, p, indexOfPivot - 1);
+		count++;
 		quickSort(array, indexOfPivot + 1, r);
+		count++;
 	}
 }
